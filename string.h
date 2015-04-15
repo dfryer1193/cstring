942a2c57c5fe9f789538160e1d5f609e27001515
David Fryer <dfryer1193@gmail.com>
2015-04-15 11:41:20 -0400

    Rewrote to pass in objects, not pointers.

--- string.h
+++ string.h
@@ -11,26 +11,26 @@ struct String {
   char* str;
 
   // Constructor
-  String* (*init)     (char* val);
+  String  (*init)     (String working, char* val);
 
   // Methods
-  String* (*substr)   (String* self, int head, int tail);
-  String* (*find)     (String* self, String* sub);
-  String* (*append)   (String* self, String* postfix);
-  int     (*findchar) (String* self, char character, int index);
-  int     (*contains) (String* self, String* str);
-  char*   (*toString) (String* self);
-  void    (*destroy)  (String* self);
+  String* (*substr)   (String self, int head, int tail);
+  String* (*find)     (String self, String sub);
+  int     (*findchar) (String self, char character, int index);
+  int     (*contains) (String self, String str);
+  char*   (*toString) (String self);
+  void    (*destroy)  (String self);
+  void    (*append)   (String self, String* postfix);
 };
 
 String* String_new      (size_t size, String proto, char* val);
-String* String_init     (char* val);
-void    String_destroy  (String* self);
+String  String_init     (String self, char* val);
+void    String_destroy  (String self);
 
-String* String_find     (String* self, String* sub);
-String* String_substr   (String* self, int head, int tail);
-void    String_append   (String* self, String* str);
-int     String_findchar (String* self, char character, int index);
-int     String_contains (String* self, String* sub);
+String* String_find     (String self, String sub);
+String* String_substr   (String self, int head, int tail);
+void    String_append   (String self, String str);
+int     String_findchar (String self, char character, int index);
+int     String_contains (String self, String sub);
 
 #endif
